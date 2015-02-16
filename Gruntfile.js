40b9644da1fa034e5559349d9b5789133ae120c4
constantine-cz <ludvar.jiri@gmail.com>
2015-02-16 21:52:06 +0100

    First commit, preparation of files and structure

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        less: {
+            prod: {
+                options: {
+                    paths: ["css"],
+                    sourceMap: true,
+                    plugins: [
+                        new (require('less-plugin-autoprefix'))({browsers: ["last 5 versions"]}),
+                        new (require('less-plugin-clean-css'))()
+                    ]
+                },
+                files: {
+                    "css/style.css": "less/*.less"
+                }
+            }
+        },
+        watch: {
+            files: ['less/*.less'],
+            tasks: ['less', 'postcss']
+        }
+    });
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // Default task(s).
+    grunt.registerTask('default', ['less']);
+};
